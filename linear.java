dc4208664e6fcaa275476bbfdead6495dffbbb55
bhushansh26 <bhushanshelke30@gmail.com>
2023-02-02 15:15:02 +0530

    linear.java updated

--- linear.java
+++ linear.java
@@ -1,6 +1,17 @@
-class Linear{
+class linear{
     public static void main(String args[])
     {
-        System.out.println("Linear code here");
+       Switch n
+        {
+            case 1:
+                System.out.println("Linear code here");
+                break;
+            case 2:
+                System.out.println("Binary code here");
+                break;
+            default:
+                System.out.println("Enter valid number");
+
+        }
     }
 }
